b081a8b275838757970a252f745ce9d32012f651
Domey Benjamin <domeybenjamin1@gmail.com>
2023-12-26 13:47:23 +0000

    feat: added rekognition meta_data

--- services/main/Models/RekognitionMetaData.cs
+++ services/main/Models/RekognitionMetaData.cs
@@ -6,7 +6,14 @@ namespace main.Models;
 public class RekognitionMetaData
 {
 
-    public required string Status { get; set; } = "PENDING"; // PENDING, SUCCESS, FAILED
+    public required string Status { get; set; } = "PENDING"; // PENDING, INDEXED, NOT_INDEXED, FAILED
+    
+    [BsonElement("error_details")]
+    public BsonDocument? ErrorDetails { get; set; }
 
-    // TODO: Cross check with api docs and see what kind of data is returned
+    [BsonElement("image_id")]
+    public string? ImageId { get; set; }
+    
+    [BsonElement("face_id")]
+    public string? FaceId { get; set; }
 }
