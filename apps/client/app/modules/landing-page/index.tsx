9a30f2a22f6d3cd4c6a968a438e7ecfe759c1515
Fiifi Foh Arkhurst Jr <42737537+arkhurst@users.noreply.github.com>
2024-01-03 16:05:02 +0000

    added some updates on the landing page (#9)

--- apps/client/app/modules/landing-page/index.tsx
+++ apps/client/app/modules/landing-page/index.tsx
@@ -1,48 +1,49 @@
-import { Header } from "@/components/layout/index.ts"
-import { HeroSection } from "./components/hero-section/index.tsx"
-import { useInView } from 'react-intersection-observer';
-import { Content } from "@/components/Content/index.tsx";
+import {Header} from '@/components/layout/index.ts'
+import {HeroSection} from './components/hero-section/index.tsx'
+import {useInView} from 'react-intersection-observer'
+import {Content} from '@/components/Content/index.tsx'
+import {FadeIn, FadeInStagger} from '@/components/animation/FadeIn.tsx'
+import {Fragment} from 'react'
 
 const imageUrls = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg",
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg',
+  'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg',
 ]
 
 export const LandingPageModule = () => {
-    const { ref: heroRef, inView, } = useInView({
-        threshold: 0,
-    });
+  const {ref: heroRef, inView} = useInView({
+    threshold: 0,
+  })
 
-    return (
-        <div className="relative">
-            <div ref={heroRef}>
-                <HeroSection />
-            </div>
-            {
-                inView ? null : (
-                    <Header isHeroSearchInVisible={false} />
-                )
-            }
-
-            <div className="mx-auto  max-w-8xl items-center my-10 px-3 sm:px-3 md:px-8">
-                <div className="columns-1 gap-2 sm:columns-2 sm:gap-4 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8 ">
-                    {
-                        imageUrls.map((url, index) => (
-                            <Content key={index} content={{ url }} />
-                        ))
-                    }
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="relative">
+      <div ref={heroRef}>
+        <HeroSection />
+      </div>
+      {inView ? null : <Header isHeroSearchInVisible={false} />}
+      <div className="mx-auto  max-w-8xl items-center my-10 px-3 sm:px-3 md:px-8">
+        <FadeInStagger faster>
+          <div className="columns-1 gap-2 sm:columns-2 sm:gap-4 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8 ">
+            {imageUrls.map((url, index) => (
+              <Fragment key={index}>
+                <FadeIn>
+                  <Content content={{url}} />
+                </FadeIn>
+              </Fragment>
+            ))}
+          </div>
+        </FadeInStagger>
+      </div>
+    </div>
+  )
 }
