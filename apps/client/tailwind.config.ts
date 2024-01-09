05750e05ca05296ad0b7262cbdf4d9a5605d9dca
arkhurst <fiiarkhurst@gmail.com>
2024-01-09 19:30:38 +0000

    added custom fonts

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -3,7 +3,11 @@ import {type Config} from 'tailwindcss'
 export default {
   content: ['./app/**/*.+(js|jsx|ts|tsx|mdx|md)'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 } satisfies Config
