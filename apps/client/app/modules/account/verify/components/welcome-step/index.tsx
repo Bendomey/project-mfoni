be47d2bed1581574fdec36e94d56df4fa7f6fc69
Domey Benjamin <domeybenjamin1@gmail.com>
2024-01-22 09:35:57 +0000

    feat: added success step

--- apps/client/app/modules/account/verify/components/welcome-step/index.tsx
+++ apps/client/app/modules/account/verify/components/welcome-step/index.tsx
@@ -1,3 +1,22 @@
+import Confetti from 'react-confetti'
+import {Button} from '@/components/button/index.tsx'
+
 export const WelcomeStep = () => {
-  return <>Welcome</>
+  return (
+    <>
+      {/** @ts-expect-error - Confetti default export errors. */}
+      <Confetti
+        className='h-full w-full'
+      />
+      <div className='text-7xl mb-16'>🚀</div>
+      <h1 className='text-2xl font-extrabold mb-2'>You&apos;re now a creator!</h1>
+      <p>
+        You&apos;ve successfully verified your account. You can now start uploading content.
+      </p>
+
+      <Button externalClassName="mt-5" size="lg">
+        Explore App
+      </Button>
+    </>
+  )
 }
