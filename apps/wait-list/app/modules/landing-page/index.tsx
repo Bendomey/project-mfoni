46ddff583388652ae0680b24bb5f801af33fd2da
Fiifi Foh Arkhurst Jr <42737537+arkhurst@users.noreply.github.com>
2024-02-29 17:15:07 +0000

    completed landing page (#60)

--- apps/wait-list/app/modules/landing-page/index.tsx
+++ apps/wait-list/app/modules/landing-page/index.tsx
@@ -1,3 +1,34 @@
+import {PhotoGrid} from '@/components/image-slider/index.jsx'
+import {BackgroundContainer} from '@/components/layout/container.tsx'
+import {Header} from '@/components/layout/header.tsx'
+import {MovingBorderButton} from '@/components/moving-border/index.tsx'
+
 export const LandingPageModule = () => {
-  return <div className="relative ">hello world</div>
+  return (
+    <BackgroundContainer>
+      <div className="h-[60dvh] md:h-[50vh] lg:h-[50vh] flex flex-col justify-center items-center">
+        <Header />
+        <div className="max-w-4xl pt-8 md:pt-0 lg:pt-0 flex flex-col justify-center items-center   mx-auto px-4">
+          <h1 className="relative z-10 text-2xl md:text-4xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-500 to-neutral-900 leading-8 lg:leading-tight text-center font-sans font-bold">
+            The best photo gallery shared by creators in{' '}
+            <strong className="text-blue-600">Ghana.</strong>
+          </h1>
+          <p className="text-neutral-500 max-w-lg mx-auto my-6 text-base md:text-lg  leading-7 lg:leading-8 text-center relative z-10">
+            We have been working hard to bring you the best photo gallery shared
+            by creators in Ghana. We hope you enjoy it!
+          </p>
+          <MovingBorderButton
+            borderRadius="1.25rem"
+            className="bg-white text-base font-medium text-black border-gray-200 border dark:bg-slate"
+          >
+            Join waitlist{' '}
+            <span className="pl-1" aria-hidden="true">
+              &rarr;
+            </span>
+          </MovingBorderButton>
+        </div>
+      </div>
+      <PhotoGrid />
+    </BackgroundContainer>
+  )
 }
--- apps/wait-list/tailwind.config.ts
+++ apps/wait-list/tailwind.config.ts
@@ -1,13 +1,83 @@
+/* eslint-disable import/no-named-default */
+/* eslint-disable func-names */
 import {type Config} from 'tailwindcss'
+import {default as flattenColorPalette} from 'tailwindcss/lib/util/flattenColorPalette.js'
+
+import svgToDataUri from 'mini-svg-data-uri'
 
 export default {
   content: ['./app/**/*.+(js|jsx|ts|tsx|mdx|md)'],
+  // darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         sans: ['Plus Jakarta Sans', 'sans-serif'],
       },
+      animation: {
+        'fade-in': 'fade-in 0.5s linear forwards',
+        marquee: 'marquee var(--marquee-duration) linear infinite',
+        'spin-slow': 'spin 4s linear infinite',
+        'spin-slower': 'spin 6s linear infinite',
+        'spin-reverse': 'spin-reverse 1s linear infinite',
+        'spin-reverse-slow': 'spin-reverse 4s linear infinite',
+        'spin-reverse-slower': 'spin-reverse 6s linear infinite',
+      },
+      keyframes: {
+        'fade-in': {
+          from: {
+            opacity: '0',
+          },
+          to: {
+            opacity: '1',
+          },
+        },
+        marquee: {
+          '100%': {
+            transform: 'translateY(-50%)',
+          },
+        },
+        'spin-reverse': {
+          to: {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [
+    function addVariablesForColors({addBase, theme}: any) {
+      const allColors = flattenColorPalette(theme('colors'))
+      const newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+      )
+
+      addBase({
+        ':root': newVars,
+      })
+    },
+    function ({matchUtilities, theme}: any) {
+      matchUtilities(
+        {
+          'bg-grid': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          'bg-grid-small': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          'bg-dot': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+            )}")`,
+          }),
+        },
+        {values: flattenColorPalette(theme('backgroundColor')), type: 'color'},
+      )
+    },
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 } satisfies Config
