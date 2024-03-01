9c3ef31b629ade6d8811caf99ecd2472376bbd10
Domey Benjamin <domeybenjamin1@gmail.com>
2024-03-01 15:32:20 +0000

    chore: remove server env

--- apps/wait-list/remix.config.cjs
+++ apps/wait-list/remix.config.cjs
@@ -5,4 +5,5 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  tailwind: true
 }
--- apps/wait-list/server/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import * as express from 'express'
-
-const router = express.Router()
-
-export {router}
