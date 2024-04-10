5ac335ab93ec1cc834fd279848b4398142c57f8b
NanaKay007 <jnranikuabe@gmail.com>
2024-04-09 21:29:48 -0700

    fix a few things

--- services/main/Tests.L0/ControllerTests/WaitlistControllerTests.cs
+++ services/main/Tests.L0/ControllerTests/WaitlistControllerTests.cs
@@ -1,47 +1,51 @@
-namespace Tests.L0;
-
-using main.Controllers;
-using main.DTOs;
-using main.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging.Abstractions;
-using Moq;
-
-[TestClass]
-public class WaitlistControllerTests
+namespace Tests.L0
 {
-    private readonly MockRepository mockRepository;
-    private readonly WaitlistController waitlistController;
-    private readonly Mock<WaitlistService> waitlistServiceMock;
 
-    public WaitlistControllerTests()
+    using main.Controllers;
+    using main.DTOs;
+    using main.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging.Abstractions;
+    using Moq;
+
+    [TestClass]
+    public class WaitlistControllerTests
     {
-        this.mockRepository = new MockRepository(MockBehavior.Strict);
-        this.waitlistServiceMock = this.mockRepository.Create<WaitlistService>();
-        this.waitlistController = new WaitlistController(NullLogger<WaitlistController>.Instance, this.waitlistServiceMock.Object);
-    }
+        private readonly MockRepository mockRepository;
+        private readonly WaitlistController waitlistController;
+        private readonly Mock<WaitlistService> waitlistServiceMock;
 
-    [TestMethod]
-    public async Task AddWaitlistUser_CallsWaitlistService()
-    {
-        var sampleRequest = new CreateWaitlistInput(){
-            Name = "Nana",
-            PhoneNumber = "0558829926",
-            Email = "hello@gmail.com",
-            UserType = "client",
-        };
+        public WaitlistControllerTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+            this.waitlistServiceMock = this.mockRepository.Create<WaitlistService>();
+            this.waitlistController = new WaitlistController(NullLogger<WaitlistController>.Instance, this.waitlistServiceMock.Object);
+        }
 
-        this.waitlistServiceMock
-            .Setup(m => m.SaveWaitlistDetails(sampleRequest))
-            .ReturnsAsync(new Waitlist(){
-                Name = sampleRequest.Name,
-                PhoneNumber = sampleRequest.PhoneNumber,
-                Type = sampleRequest.UserType,
-                Email = sampleRequest.Email
-            });
+        [TestMethod]
+        public async Task AddWaitlistUser_CallsWaitlistService()
+        {
+            var sampleRequest = new CreateWaitlistInput()
+            {
+                Name = "Nana",
+                PhoneNumber = "0558829926",
+                Email = "hello@gmail.com",
+                UserType = "client",
+            };
 
-        var result = await this.waitlistController.AddWaitlistUser(sampleRequest);
+            this.waitlistServiceMock
+                .Setup(m => m.SaveWaitlistDetails(sampleRequest))
+                .ReturnsAsync(new Waitlist()
+                {
+                    Name = sampleRequest.Name,
+                    PhoneNumber = sampleRequest.PhoneNumber,
+                    Type = sampleRequest.UserType,
+                    Email = sampleRequest.Email
+                });
 
-        Assert.AreEqual(200, (result as ObjectResult)?.StatusCode);
+            var result = await this.waitlistController.AddWaitlistUser(sampleRequest);
+
+            Assert.AreEqual(200, (result as ObjectResult)?.StatusCode);
+        }
     }
 }
