34a43d253a683374ce5b4d8413b3893563a0170f
Ben Domey <41929243+Bendomey@users.noreply.github.com>
2024-09-05 09:31:48 +0000

    feat: send emails to waitlist applicant (#85)
    * chore: sending emails now with on our waitlist
    
    * chore: added linting changes

---
+++ services/main/Configurations/EmailConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace main.Configurations;
+
+public class SendEmailInput
+{
+    public required string From { get; set; }
+    public required string Email { get; set; }
+    public required string Subject { get; set; }
+    public required string Message { get; set; }
+    public required string ApiKey { get; set; }
+}
+
+public class EmailConfiguration
+{
+    public static async Task Send(SendEmailInput input)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string apiKey = input.ApiKey;
+
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            var emailData = new
+            {
+                from = input.From,
+                to = new[] { input.Email },
+                subject = input.Subject,
+                text = input.Message
+            };
+
+            var jsonContent = JsonConvert.SerializeObject(emailData);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("https://api.resend.com/emails", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Email sent successfully: " + await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                Console.WriteLine($"Failed to send email. Status code: {response.StatusCode}");
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
+
---
+++ services/main/Lib/EmailTemplates.cs
@@ -0,0 +1,31 @@
+
+
+
+public class EmailTemplates
+{
+    public static string WaitlistSubject = "You're in! Welcome to the mfoni crew 📸";
+    public static string WaitlistBody = @"
+Hey there!
+
+Guess what? You're now part of the mfoni fam! 🎉 Thanks for joining our waitlist.
+
+What's coming your way:
+• A cool new way to find and share Ghana's best photos
+• Early access to the app (yep, you're VIP!)
+• First dibs on some sweet launch deals
+
+We're cooking up something awesome and can't wait to show you.
+
+Spread the love! Tell your friends to join the waitlist at [Waitlist URL].
+
+Got questions? Hit us up at info@mfoni.app.
+
+Stay tuned and keep snapping!
+
+The mfoni Team
+P.S. Let's hang out on socials:
+Twitter(https://twitter.com/mfoniapp)
+
+#mfoniComingSoon #GhanaPhotography #SnapShareDiscover
+    ";
+}
