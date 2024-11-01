576bdb1068ab5275e3c116ff6301557f27b9f9f2
Domey Benjamin <domeybenjamin1@gmail.com>
2024-11-01 11:50:19 +0000

    chore: bring back swagger for staging

--- services/main/Program.cs
+++ services/main/Program.cs
@@ -163,11 +163,12 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// TODO: bring this back when we go to production.
+// if (!app.Environment.IsProduction())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 // app.UseHttpsRedirection();
 
