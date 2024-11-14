e9f0a47881c2882387007cbb6694292021f173ed
gidoo5331 <gideonbempong533@gmail.com>
2024-11-14 15:57:52 +0000

    formatted

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    images: {
-        domains: [
-    'staging-mfoni.s3.us-east-1.amazonaws.com'
-        ]}
+  output: "standalone",
+  images: {
+    domains: ["staging-mfoni.s3.us-east-1.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
