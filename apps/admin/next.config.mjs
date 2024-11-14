d07bb9216beb18201a4ebceb49cfbba094cbaac4
gidoo5331 <gideonbempong533@gmail.com>
2024-11-14 15:44:13 +0000

    added modal

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
+    output: 'standalone',
+    images: {
+        domains: [
+    'staging-mfoni.s3.us-east-1.amazonaws.com'
+        ]}
 };
 
 export default nextConfig;
