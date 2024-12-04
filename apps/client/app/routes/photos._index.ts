da77b826e3b8a5e9fe69a56ed6cb4b73e6f0a4b6
Domey Benjamin <domeybenjamin1@gmail.com>
2024-12-04 14:12:32 +0000

    chore: removed some meta info

--- apps/client/app/routes/photos._index.ts
+++ apps/client/app/routes/photos._index.ts
@@ -1,6 +1,7 @@
 import {PAGES} from '@/constants/index.ts'
 import {redirect} from '@remix-run/node'
 
+// This is a 404 page. but because it's a search page, we redirect to the home page
 export async function loader() {
   return redirect(PAGES.HOME)
 }
