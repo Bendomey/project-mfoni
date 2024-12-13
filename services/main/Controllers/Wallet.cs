9fd0f1f130b2365ac11b23105307585cb57c9e06
Domey Benjamin <domeybenjamin1@gmail.com>
2024-12-13 09:56:04 +0000

    chore: added admin wallet endpoint

--- services/main/Controllers/Wallet.cs
+++ services/main/Controllers/Wallet.cs
@@ -18,17 +18,20 @@ public class WalletController : ControllerBase
 {
     private readonly ILogger<UserController> logger;
     private readonly WalletService walletService;
+    private readonly AdminWalletService _adminWalletService;
     private readonly WalletTransactionTransformer _walletTransactionTransformer;
 
     public WalletController(
         ILogger<UserController> logger,
         WalletService walletService,
-        WalletTransactionTransformer walletTransactionTransformer
+        WalletTransactionTransformer walletTransactionTransformer,
+        AdminWalletService adminWalletService
     )
     {
         this.logger = logger;
         this.walletService = walletService;
         this._walletTransactionTransformer = walletTransactionTransformer;
+        this._adminWalletService = adminWalletService;
     }
 
     /// <summary>
@@ -52,6 +55,7 @@ public class WalletController : ControllerBase
         StatusCodes.Status500InternalServerError,
         Type = typeof(StatusCodeResult)
     )]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> GetWalletTransactions(
         [FromQuery] string? type,
         [FromQuery] string? populate,
@@ -104,7 +108,177 @@ public class WalletController : ControllerBase
         }
         catch (Exception e)
         {
-            logger.LogError($"An error occured {e}");
+            logger.LogError($"Failed to fetch wallet transactions for users. Exception: {e}");
+            SentrySdk.ConfigureScope(scope =>
+            {
+                scope.SetTags(new Dictionary<string, string>
+                {
+                    {"action", "Wallet transactions for admin"},
+                    {"type", StringLib.SafeString(type)},
+                    {"populate", StringLib.SafeString(populate)},
+                    {"page", StringLib.SafeString(page.ToString())},
+                    {"pageSize", StringLib.SafeString(pageSize.ToString())},
+                    {"sort", StringLib.SafeString(sort)},
+                    {"sortBy", StringLib.SafeString(sortBy)}
+
+                });
+                SentrySdk.CaptureException(e);
+            });
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+
+    /// <summary>
+    /// Retrieves all wallet transactions of an admin
+    /// </summary>
+    /// <param name="type">Can be `DEPOSIT` or `WITHDRAW`</param>
+    /// <param name="populate">Comma separated values to populate fields</param>
+    /// <param name="page">The page to be navigated to</param>
+    /// <param name="pageSize">The number of items on a page</param>
+    /// <param name="sort">To sort response data either by `asc` or `desc`</param>
+    /// <param name="sortBy">What field to sort by.</param>
+    /// <response code="200">Transactions Retrieved Successfully</response>
+    /// <response code="500">An unexpected error occured</response>
+    [Authorize(Policy = "admin")]
+    [HttpGet("admins/wallet-transactions")]
+    [ProducesResponseType(
+        StatusCodes.Status200OK,
+        Type = typeof(ApiEntityResponse<EntityWithPagination<OutputWalletTransaction>>)
+    )]
+    [ProducesResponseType(
+        StatusCodes.Status500InternalServerError,
+        Type = typeof(StatusCodeResult)
+    )]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetWalletTransactionsForAdmin(
+        [FromQuery] string? type,
+        [FromQuery] string? populate,
+        [FromQuery] int? page,
+        [FromQuery] int? pageSize,
+        [FromQuery] string? sort,
+        [FromQuery] string sortBy = "created_at"
+    )
+    {
+        try
+        {
+            logger.LogInformation("Getting all wallet transactions for admin");
+            var queryFilter = HttpLib.GenerateFilterQuery<WalletTransaction>(page, pageSize, sort, sortBy, populate);
+            var adminWallet = await _adminWalletService.Get();
+
+            var input = new GetWalletTransactionsInput
+            {
+                UserId = adminWallet.Id,
+                Type = type
+            };
+            var transactions = await walletService.GetWalletTransactions(queryFilter, input);
+            long count = await walletService.CountWalletTransactions(input);
+
+            var outputTransactions = new List<OutputWalletTransaction>();
+            foreach (var transaction in transactions)
+            {
+                outputTransactions.Add(await _walletTransactionTransformer.Transform(transaction, populate: queryFilter.Populate));
+            }
+
+            var response = HttpLib.GeneratePagination<OutputWalletTransaction, WalletTransaction>(
+                outputTransactions,
+                count,
+                queryFilter
+            );
+
+            return new ObjectResult(
+                new GetEntityResponse<EntityWithPagination<OutputWalletTransaction>>(response, null).Result()
+            )
+            {
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+        catch (HttpRequestException e)
+        {
+            var statusCode = e.StatusCode ?? HttpStatusCode.BadRequest;
+            return new ObjectResult(new GetEntityResponse<AnyType?>(null, e.Message).Result())
+            {
+                StatusCode = (int)statusCode
+            };
+        }
+        catch (Exception e)
+        {
+            logger.LogError($"Failed to fetch wallet transactions for admin. Exception: {e}");
+            SentrySdk.ConfigureScope(scope =>
+            {
+                scope.SetTags(new Dictionary<string, string>
+                {
+                    {"action", "Wallet transactions for admin"},
+                    {"type", StringLib.SafeString(type)},
+                    {"populate", StringLib.SafeString(populate)},
+                    {"page", StringLib.SafeString(page.ToString())},
+                    {"pageSize", StringLib.SafeString(pageSize.ToString())},
+                    {"sort", StringLib.SafeString(sort)},
+                    {"sortBy", StringLib.SafeString(sortBy)}
+
+                });
+                SentrySdk.CaptureException(e);
+            });
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    /// <summary>
+    /// Retrieves wallet of an admin
+    /// </summary>
+    /// <response code="200">Transaction balance Successfully</response>
+    /// <response code="500">An unexpected error occured</response>
+    [Authorize(Policy = "admin")]
+    [HttpGet("admins/wallet")]
+    [ProducesResponseType(
+        StatusCodes.Status200OK,
+        Type = typeof(ApiEntityResponse<OutputAdminWallet>)
+    )]
+    [ProducesResponseType(
+        StatusCodes.Status500InternalServerError,
+        Type = typeof(StatusCodeResult)
+    )]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetWalletForAdmin()
+    {
+        try
+        {
+            logger.LogInformation("Getting wallet for admin");
+            var adminWallet = await _adminWalletService.Get();
+
+            return new ObjectResult(
+                new GetEntityResponse<OutputAdminWallet>(new OutputAdminWallet
+                {
+                    Id = adminWallet.Id,
+                    Wallet = adminWallet.Wallet,
+                    BookWallet = adminWallet.BookWallet,
+                    CreatedAt = adminWallet.CreatedAt,
+                    UpdatedAt = adminWallet.UpdatedAt
+                }, null).Result()
+            )
+            {
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+        catch (HttpRequestException e)
+        {
+            var statusCode = e.StatusCode ?? HttpStatusCode.BadRequest;
+            return new ObjectResult(new GetEntityResponse<AnyType?>(null, e.Message).Result())
+            {
+                StatusCode = (int)statusCode
+            };
+        }
+        catch (Exception e)
+        {
+            logger.LogError($"Failed to fetch wallet for admin. Exception: {e}");
+            SentrySdk.ConfigureScope(scope =>
+            {
+                scope.SetTags(new Dictionary<string, string>
+                {
+                    {"action", "Wallet transactions for admin"},
+                });
+                SentrySdk.CaptureException(e);
+            });
             return new StatusCodeResult(StatusCodes.Status500InternalServerError);
         }
     }
