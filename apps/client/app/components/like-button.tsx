6a6f25caae3cc564862170ec314c219abf5d74f1
Domey Benjamin <domeybenjamin1@gmail.com>
2024-12-13 15:02:01 +0000

    chore: invalidate queries after liking content

--- apps/client/app/components/like-button.tsx
+++ apps/client/app/components/like-button.tsx
@@ -1,6 +1,9 @@
 import { useFetcher, useLocation } from '@remix-run/react'
+import { useQueryClient } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
+import { QUERY_KEYS } from '@/constants/index.ts'
 import { errorToast } from '@/lib/custom-toast-functions.tsx'
+import { useAuth } from '@/providers/auth/index.tsx'
 
 interface Props {
 	content: Content
@@ -12,6 +15,8 @@ interface Props {
 }
 
 export function LikeButton({ content, children }: Props) {
+	const { currentUser } = useAuth()
+	const queryClient = useQueryClient()
 	const location = useLocation()
 	const fetcher = useFetcher<{ error: string }>()
 	const [isLiked, setIsLiked] = useState(Boolean(content.currentUserLike))
@@ -40,6 +45,12 @@ export function LikeButton({ content, children }: Props) {
 			},
 		)
 		setIsLiked((prev) => !prev)
+
+		if (currentUser) {
+			queryClient.invalidateQueries({
+				queryKey: [QUERY_KEYS.CONTENT_LIKES, 'user', currentUser.id]
+			})
+		}
 	}
 
 	const isDisabled = fetcher.state === 'submitting'
