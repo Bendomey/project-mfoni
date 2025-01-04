1d230c15615d3c65640b3a2ee70c607495ccf7eb
gidoo5331 <gideonbempong533@gmail.com>
2025-01-04 11:45:14 +0000

    add modal

---
+++ apps/admin/modules/feature/index.tsx
@@ -0,0 +1,85 @@
+import { Button } from "@/components/ui/button";
+import { Dispatch, SetStateAction } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogOverlay,
+} from "@/components/ui/dialog";
+import { useToast } from "@/hooks/use-toast";
+import { useFeatureCollection } from "@/api";
+import { Loader2Icon } from "lucide-react";
+
+interface FeatureCollectionModalProps {
+  data?: Collection;
+  refetch: VoidFunction;
+  opened: boolean;
+  setOpened: Dispatch<SetStateAction<boolean>>;
+}
+
+export const FeatureCollectionModal = ({
+  data,
+  refetch,
+  opened,
+  setOpened,
+}: FeatureCollectionModalProps) => {
+  const { toast } = useToast();
+  const { mutate, isPending: isLoading } = useFeatureCollection();
+
+  const handleSubmit = () => {
+    if (!data) {
+      toast({
+        title: "Collection data not found",
+        variant: "destructive",
+      })
+      return;
+    }
+
+      mutate(
+        data.id,
+        {
+          onError: () => {
+            toast({
+              title: "We couldn't process your request",
+              variant: "destructive",
+            })   
+            
+          }, onSuccess: () => {
+          refetch()
+          toast({
+            title: "Collection marked as featured",
+            variant: "success",
+          });
+          setOpened(false);
+        }
+      }
+      )
+  };
+
+  const name = <span className="capitalize">{data?.name}'s </span> 
+
+  return (
+    <Dialog open={opened} onOpenChange={() => setOpened(false)}>
+      <DialogOverlay className="bg-black/10" />
+      <DialogContent className="sm:max-w-[425px] bg-white dark:bg-black top-32">
+        <DialogHeader>
+          <DialogTitle className="leading-normal">
+          Feature {data?.name ? name : "this"} collection
+          </DialogTitle>
+          <DialogDescription className="text-gray-600 dark:text-gray-400">
+            Are you sure you want to mark{" "}
+            {data?.name ? name : "this"} collection as featured?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button className="bg-emerald-600 hover:bg-emerald-400" disabled={isLoading} type="button" onClick={() => handleSubmit()}>
+          { isLoading ? <Loader2Icon className="animate-spin" /> : null} Yes, mark as featured
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
