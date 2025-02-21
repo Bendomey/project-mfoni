90afd36fd0df894d1d72f72afa385a076dfbd9e8
Domey Benjamin <domeybenjamin1@gmail.com>
2025-02-21 11:38:44 +0000

    chore: added visual search page

---
+++ apps/client/app/modules/search/visual/components/header.tsx
@@ -0,0 +1,119 @@
+import { PhotoIcon } from '@heroicons/react/20/solid'
+import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline'
+import { Link, useLocation, useParams, useSearchParams } from '@remix-run/react';
+import { useMemo } from 'react';
+import { FiltersDialog } from "../../components/filters-dialog/index.tsx";
+import { FilterByLicense } from '../../components/license-select/index.tsx';
+import { FilterByOrientation } from '../../components/orientation-select/index.tsx';
+import { Button } from "@/components/button/index.tsx";
+import { PAGES } from '@/constants/index.ts';
+import { useDisclosure } from "@/hooks/use-disclosure.tsx";
+import { classNames } from '@/lib/classNames.ts';
+import { safeString } from '@/lib/strings.ts';
+
+export function VisualHeader() {
+    const { query: queryParam } = useParams()
+    const location = useLocation()
+    const filterModalState = useDisclosure()
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const tabs = useMemo(
+        () => [
+            {
+                name: 'Photos',
+                href: PAGES.SEARCH.PHOTOS.replace(':query', safeString(queryParam)),
+                icon: PhotoIcon,
+                current:
+                    encodeURI(
+                        PAGES.SEARCH.VISUAL.replace(':query', safeString(queryParam)),
+                    ) === location.pathname,
+            },
+        ],
+        [location.pathname, queryParam],
+    )
+
+    const showClearButton =
+        searchParams.get('license') || searchParams.get('orientation')
+
+    const clearFilters = () => {
+        searchParams.delete('license')
+        searchParams.delete('orientation')
+        setSearchParams(searchParams)
+    }
+
+    return (
+        <>
+            <div>
+                <div className="">
+                    <div className="flex items-center justify-between border-b border-gray-200">
+                        <nav aria-label="Tabs" className="-mb-px flex space-x-8">
+                            {tabs.map((tab) => (
+                                <Link
+                                    prefetch="intent"
+                                    key={tab.name}
+                                    to={tab.href}
+                                    aria-current={tab.current ? 'page' : undefined}
+                                    className={classNames(
+                                        tab.current
+                                            ? 'border-blue-500 text-blue-600'
+                                            : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                                        'group inline-flex items-center border-b-2 px-1 py-3 text-sm font-medium',
+                                    )}
+                                >
+                                    <tab.icon
+                                        aria-hidden="true"
+                                        className={classNames(
+                                            tab.current
+                                                ? 'text-blue-500'
+                                                : 'text-gray-400 group-hover:text-gray-500',
+                                            '-ml-0.5 mr-2 size-5',
+                                        )}
+                                    />
+                                    <span>{tab.name}</span>
+                                    <span
+											className={classNames(
+												tab.current
+													? 'bg-blue-100 text-blue-600'
+													: 'bg-gray-100 text-gray-900',
+												'ml-1 hidden rounded-full px-2.5 py-0.5 text-xs font-medium md:inline-block',
+											)}
+										>
+											1k
+										</span>
+                                </Link>
+                            ))}
+                        </nav>
+                        <div className="hidden flex-row items-center gap-3 lg:flex">
+                            {showClearButton ? (
+                                <button
+                                    onClick={clearFilters}
+                                    className="text-xs text-gray-500"
+                                >
+                                    Clear
+                                </button>
+                            ) : null}
+
+                            <FilterByLicense />
+                            <FilterByOrientation />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5 flex justify-end lg:hidden">
+                <Button
+                    onClick={filterModalState.onOpen}
+                    size="sm"
+                    color="secondaryGhost"
+                >
+                    <AdjustmentsHorizontalIcon className="mr-2 h-auto w-5" /> Filters{' '}
+                    {searchParams.size > 0 ? `(${searchParams.size})` : ''}
+                </Button>
+            </div>
+
+            <FiltersDialog
+                isOpened={filterModalState.isOpened}
+                onClose={filterModalState.onClose}
+            />
+        </>
+    );
+}
---
+++ apps/client/app/routes/search_.visual.$query.ts
@@ -0,0 +1,65 @@
+import { type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
+import { dehydrate, QueryClient } from '@tanstack/react-query'
+import { searchVisualContents } from '@/api/contents/index.ts'
+import { QUERY_KEYS } from '@/constants/index.ts'
+import { environmentVariables } from '@/lib/actions/env.server.ts'
+import { extractAuthCookie } from '@/lib/actions/extract-auth-cookie.ts'
+import { jsonWithCache } from '@/lib/actions/json-with-cache.server.ts'
+import {
+	getDisplayUrl,
+	getDomainUrl,
+	validateLicense,
+	validateOrientation,
+} from '@/lib/misc.ts'
+import { getSocialMetas } from '@/lib/seo.ts'
+import { safeString } from '@/lib/strings.ts'
+import { SearchVisualModule } from '@/modules/search/visual/index.tsx'
+
+export async function loader(loaderArgs: LoaderFunctionArgs) {
+	const queryClient = new QueryClient()
+
+	const authCookie = await extractAuthCookie(
+		loaderArgs.request.headers.get('cookie'),
+	)
+
+	const baseUrl = `${environmentVariables().API_ADDRESS}/api`
+
+	const query = {
+		pagination: { page: 0, per: 50 },
+		filters: {
+			license: validateLicense(loaderArgs.params.license ?? 'ALL'),
+			orientation: validateOrientation(loaderArgs.params.orientation ?? 'ALL'),
+			mediaKey: safeString(loaderArgs.params.query),
+		},
+		populate: ['content.createdBy'],
+	}
+
+	await queryClient.prefetchQuery({
+		queryKey: [QUERY_KEYS.CONTENTS, 'visual-search', query],
+		queryFn: () =>
+			searchVisualContents(query, {
+				authToken: authCookie?.token,
+				baseUrl,
+			}),
+	})
+
+	const dehydratedState = dehydrate(queryClient)
+	return jsonWithCache({
+		dehydratedState,
+		origin: getDomainUrl(loaderArgs.request),
+	})
+}
+
+export const meta: MetaFunction<typeof loader> = ({ data, location }) => {
+	return getSocialMetas({
+		title: `Visual search | mfoni`,
+		description: `Download free, high-quality pictures.`,
+		url: getDisplayUrl({
+			origin: data?.origin ?? 'https://mfoni.app',
+			path: location.pathname,
+		}),
+		keywords: 'search,visual,photos,',
+	})
+}
+
+export default SearchVisualModule
