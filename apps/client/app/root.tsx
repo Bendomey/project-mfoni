0af9efb862d5b71e833171a8123015eaf0ca64a1
Domey Benjamin <domeybenjamin1@gmail.com>
2025-03-09 11:47:06 +0000

    chore: format icon link entries for consistency

--- apps/client/app/root.tsx
+++ apps/client/app/root.tsx
@@ -40,21 +40,21 @@ dayjs.extend(localizedFormat)
 export const links: LinksFunction = () => {
 	return [
 		{
-		  rel: 'apple-touch-icon',
-		  sizes: '180x180',
-		  href: '/apple-touch-icon.png',
+			rel: 'apple-touch-icon',
+			sizes: '180x180',
+			href: '/apple-touch-icon.png',
 		},
 		{
-		  rel: 'icon',
-		  type: 'image/png',
-		  sizes: '32x32',
-		  href: '/favicon-32x32.png',
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '32x32',
+			href: '/favicon-32x32.png',
 		},
 		{
-		  rel: 'icon',
-		  type: 'image/png',
-		  sizes: '16x16',
-		  href: '/favicon-16x16.png',
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '16x16',
+			href: '/favicon-16x16.png',
 		},
 		{
 			rel: 'manifest',
