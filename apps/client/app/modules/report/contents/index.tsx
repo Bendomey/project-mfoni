25100de53613109049331906aa4c7e0d7de253e9
Ben Domey <41929243+Bendomey@users.noreply.github.com>
2025-03-17 12:55:49 +0000

    chore: integrated content reports (#191)

--- apps/client/app/modules/report/contents/index.tsx
+++ apps/client/app/modules/report/contents/index.tsx
@@ -1,35 +1,111 @@
-import { Link, useLoaderData, useSearchParams } from '@remix-run/react'
-import { useMemo } from 'react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import {
+	Link,
+	useLoaderData,
+	useNavigate,
+	useSearchParams,
+} from '@remix-run/react'
+import { useEffect, useMemo } from 'react'
+import { useForm } from 'react-hook-form'
+import * as Yup from 'yup'
+import { useCreateContentReportCase } from '@/api/reports/index.ts'
 import { Button } from '@/components/button/index.tsx'
 import { Footer } from '@/components/footer/index.tsx'
 import { Header } from '@/components/layout/index.ts'
+import { errorMessagesWrapper } from '@/constants/error-messages.ts'
+import { PAGES } from '@/constants/index.ts'
 import { classNames } from '@/lib/classNames.ts'
+import { errorToast, successToast } from '@/lib/custom-toast-functions.tsx'
 import { useAuth } from '@/providers/auth/index.tsx'
 import { type loader } from '@/routes/report.contents._index.ts'
+
+interface FormValues {
+	name?: string
+	phone?: string
+	email?: string
+	contentUrl: string
+	reasonForReport: string
+	breakingLocalLaws: 'YES' | 'NOT_SURE'
+	additionalDetails?: string
+}
+
+const pathRegex = /(collections|photos|tags)\/[^/]+$/ // Accepted Path structure
 
 export function ReportContentsModule() {
 	const data = useLoaderData<typeof loader>()
 	const { isLoggedIn } = useAuth()
+	const navigate = useNavigate()
 	const [searchParams] = useSearchParams()
+	const { mutate, isPending } = useCreateContentReportCase()
 
-	const contentUrl = useMemo(() => {
+	const fullRegex = useMemo(
+		() =>
+			new RegExp(
+				`^${data.origin.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&')}/${
+					pathRegex.source
+				}`,
+			),
+		[data.origin],
+	)
+
+	const schema = useMemo(
+		() =>
+			Yup.object().shape({
+				email: Yup.string().email('Email is invalid').optional(),
+				phone: Yup.string()
+					.min(10, 'Phone number must be 10 digits or more')
+					.matches(/^[0-9]*$/, 'Phone number must be digits')
+					.when([], {
+						is: () => !isLoggedIn,
+						then: (schema) => schema.required('Phone number is required'),
+						otherwise: (schema) => schema,
+					}),
+				name: Yup.string().when([], {
+					is: () => !isLoggedIn,
+					then: (schema) => schema.required('Name is required'),
+					otherwise: (schema) => schema,
+				}),
+				contentUrl: Yup.string()
+					.matches(fullRegex, 'Invalid Content Url')
+					.required('Content Url is required'),
+				reasonForReport: Yup.string().required('Reason for report is required'),
+				breakingLocalLaws: Yup.string()
+					.oneOf(['YES', 'NOT_SURE'], 'Invalid selection')
+					.default('NOT_SURE'),
+				additionalDetails: Yup.string().when('reasonForReport', {
+					is: (val: string) => val === 'OTHER',
+					then: (schema) => schema.required('Additional details is required'),
+					otherwise: (schema) => schema,
+				}),
+			}),
+		[fullRegex, isLoggedIn],
+	)
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setValue,
+		watch,
+	} = useForm<FormValues>({
+		resolver: yupResolver(schema),
+	})
+
+	useEffect(() => {
+		let contentUrl = ''
 		let url = searchParams.get('content_url')
 		if (url) {
 			url = decodeURIComponent(url)
-			const origin = data.origin
-			const isContentUrl =
-				url.includes('/photos') ||
-				url.includes('/tags') ||
-				url.includes('/collections')
-			if (url.startsWith(origin) && isContentUrl) {
-				return url
-			}
+			contentUrl = url
 		}
 
-		return ''
-	}, [searchParams, data.origin])
+		setValue('contentUrl', contentUrl)
+	}, [data.origin, searchParams, setValue])
+
+	const contentUrl = watch('contentUrl')
 
 	const contentType = useMemo(() => {
+		if (!contentUrl) return ''
 		if (contentUrl.includes('/photos')) {
 			return 'Photo'
 		}
@@ -39,11 +115,56 @@ export function ReportContentsModule() {
 		}
 
 		if (contentUrl.includes('/collections')) {
-			return 'Collections'
+			return 'Collection'
 		}
 
 		return ''
 	}, [contentUrl])
+
+	const onSubmit = (data: FormValues) => {
+		const contentSlug = data.contentUrl.split('/')[4]
+		if (!contentSlug) {
+			errorToast('Invalid content 1', {
+				id: 'create-content-report-error',
+			})
+			return
+		}
+
+		if (!['Photo', 'Tag', 'Collection'].includes(contentType)) {
+			errorToast('Invalid content 2', {
+				id: 'create-content-report-error',
+			})
+			return
+		}
+
+		const type =
+			contentType === 'Photo'
+				? 'IMAGE'
+				: contentType === 'Collection'
+					? 'COLLECTION'
+					: 'TAG'
+
+		mutate(
+			{
+				...data,
+				contentSlug,
+				contentType: type,
+			},
+			{
+				onSuccess: async () => {
+					successToast('Report submitted successfully', {
+						id: 'create-content-report-success',
+					})
+					navigate(PAGES.HOME)
+				},
+				onError: (error) => {
+					errorToast(errorMessagesWrapper(error.message), {
+						id: 'create-content-report-error',
+					})
+				},
+			},
+		)
+	}
 
 	return (
 		<>
@@ -72,7 +193,7 @@ export function ReportContentsModule() {
 					</p>
 				</div>
 
-				<div className="mt-8 space-y-5">
+				<form onSubmit={handleSubmit(onSubmit)} className="mt-8 space-y-5">
 					<div className="rounded-lg border border-gray-200 px-5 py-7 md:px-10">
 						<p className="font-bold">Content Information</p>
 						<div className="mt-3">
@@ -81,16 +202,21 @@ export function ReportContentsModule() {
 							</label>
 							<div className="mt-2">
 								<input
-									defaultValue={contentUrl}
-									name="content-url"
 									type="text"
+									{...register('contentUrl')}
 									className={classNames(
 										'block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-blue-300 sm:text-sm/6',
+										{
+											'outline-red-500': errors.contentUrl,
+										},
 									)}
 								/>
+								<span className="text-xs text-red-600">
+									{errors.contentUrl?.message}
+								</span>
 							</div>
 
-							{contentUrl ? (
+							{contentType ? (
 								<div className="mt-2">
 									<span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1.5 text-sm font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
 										{contentType}
@@ -102,62 +228,86 @@ export function ReportContentsModule() {
 
 					<div className="space-y-3 rounded-lg border border-gray-200 px-5 py-7 md:px-10">
 						<div className="">
-							<label className="text-sm" htmlFor="content-url">
-								Reason for reporting content.
-							</label>
+							<label className="text-sm">Reason for reporting content.</label>
 							<div className="mt-2">
 								<select
-									name="content-url"
+									{...register('reasonForReport')}
 									className={classNames(
 										'block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-blue-300 sm:text-sm/6',
+										{
+											'outline-red-500': errors.reasonForReport,
+										},
 									)}
 								>
 									<option value="">Please select</option>
-									<option value="">
+									<option value="DATA_PROTECTION_AND_PRIVACY_VIOLATION">
 										Data Protection and Privacy Violations
 									</option>
-									<option value="">Pornography and Sexualized Content</option>
-									<option value="">Protection Of Minors</option>
-									<option value="">Risk of Public Security</option>
-									<option value="">Scams and Fraud</option>
-									<option value="">Unsafe and/or Illegal</option>
-									<option value="">Violence</option>
-									<option value="">Other</option>
+									<option value="PORNOGRAPHY_AND_SEXUALIZED_CONTENT">
+										Pornography and Sexualized Content
+									</option>
+									<option value="PROTECTION_OF_MINORS">
+										Protection Of Minors
+									</option>
+									<option value="PUBLIC_SECURITY">
+										Risk of Public Security
+									</option>
+									<option value="SCAMS_AND_FRAUD">Scams and Fraud</option>
+									<option value="UNSAFE_AND_ILLEGAL">
+										Unsafe and/or Illegal
+									</option>
+									<option value="VIOLENCE">Violence</option>
+									<option value="OTHER">Other</option>
 								</select>
+								<span className="text-xs text-red-600">
+									{errors.reasonForReport?.message}
+								</span>
 							</div>
 						</div>
 
 						<div className="">
-							<label className="text-sm" htmlFor="content-url">
+							<label className="text-sm">
 								Do you think this content violates a Ghanaian local law?
 							</label>
 							<div className="mt-2">
 								<select
-									name="content-url"
+									{...register('breakingLocalLaws')}
 									className={classNames(
 										'block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-blue-300 sm:text-sm/6',
+										{
+											'outline-red-500': errors.breakingLocalLaws,
+										},
 									)}
 								>
 									<option value="">Please select</option>
-									<option value="yes">Yes</option>
-									<option value="not_sure">Not Sure</option>
+									<option value="YES">Yes</option>
+									<option value="NOT_SURE">Not Sure</option>
 								</select>
+								<span className="text-xs text-red-600">
+									{errors.breakingLocalLaws?.message}
+								</span>
 							</div>
 						</div>
 
 						<div className="">
-							<label className="text-sm" htmlFor="content-url">
+							<label className="text-sm">
 								Give us additional details about this content.
 							</label>
 							<div className="mt-2">
 								<textarea
-									name="content-url"
+									{...register('additionalDetails')}
 									rows={5}
 									placeholder="Add Details Here"
 									className={classNames(
 										'block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-blue-300 sm:text-sm/6',
+										{
+											'outline-red-500': errors.additionalDetails,
+										},
 									)}
 								></textarea>
+								<span className="text-xs text-red-600">
+									{errors.additionalDetails?.message}
+								</span>
 							</div>
 						</div>
 					</div>
@@ -166,17 +316,21 @@ export function ReportContentsModule() {
 						<div className="rounded-lg border border-gray-200 px-5 py-7 md:px-10">
 							<p className="font-bold">Contact Information</p>
 							<div className="mt-3">
-								<label className="text-sm" htmlFor="content-url">
-									Name
-								</label>
+								<label className="text-sm">Name</label>
 								<div className="mt-2">
 									<input
-										name="name"
 										type="text"
+										{...register('name')}
 										className={classNames(
 											'block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-blue-300 sm:text-sm/6',
+											{
+												'outline-red-500': errors.name,
+											},
 										)}
 									/>
+									<span className="text-xs text-red-600">
+										{errors.name?.message}
+									</span>
 								</div>
 							</div>
 
@@ -186,10 +340,13 @@ export function ReportContentsModule() {
 								</label>
 								<div className="mt-2">
 									<input
-										name="phone"
+										{...register('phone')}
 										type="text"
 										className={classNames(
 											'block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-blue-300 sm:text-sm/6',
+											{
+												'outline-red-500': errors.phone,
+											},
 										)}
 									/>
 									<p
@@ -198,6 +355,9 @@ export function ReportContentsModule() {
 									>
 										Not a Ghanaian number? Provide internationalized format
 									</p>
+									<span className="text-xs text-red-600">
+										{errors.phone?.message}
+									</span>
 								</div>
 							</div>
 
@@ -207,21 +367,32 @@ export function ReportContentsModule() {
 								</label>
 								<div className="mt-2">
 									<input
-										name="email"
+										{...register('email')}
 										type="email"
 										className={classNames(
 											'block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-1 focus:-outline-offset-1 focus:outline-blue-300 sm:text-sm/6',
+											{
+												'outline-red-500': errors.email,
+											},
 										)}
 									/>
+									<span className="text-xs text-red-600">
+										{errors.email?.message}
+									</span>
 								</div>
 							</div>
 						</div>
 					)}
 
-					<Button disabled className="w-full" size="lg">
-						Submit
+					<Button
+						type="submit"
+						disabled={isPending}
+						className="w-full"
+						size="lg"
+					>
+						{isPending ? 'Submitting...' : 'Submit Report'}
 					</Button>
-				</div>
+				</form>
 			</div>
 			<Footer />
 		</>
