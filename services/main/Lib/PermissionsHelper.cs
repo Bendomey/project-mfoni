4203b025c653d8197ea881576e4e0d7828bb0c1c
Benjamin Domey <domeybenjamin1@gmail.com>
2025-04-05 20:11:34 +0000

    chore: define Unlimited constant for upload limits in PermissionsHelper

--- services/main/Lib/PermissionsHelper.cs
+++ services/main/Lib/PermissionsHelper.cs
@@ -2,8 +2,12 @@ using main.Models;
 
 namespace main.Lib;
 
+
 public static class PermissionsHelper
 {
+    public static long? Unlimited = null;
+
+
     public static long? GetNumberOfUploadsForPackageType(string packageType)
     {
         switch (packageType)
@@ -13,7 +17,7 @@ public static class PermissionsHelper
             case "BASIC":
                 return 200;
             case "ADVANCED":
-                return null;
+                return Unlimited;
             default:
                 return 0;
         }
@@ -21,6 +25,7 @@ public static class PermissionsHelper
 
     public static long? GetAmountYouCanWithdrawPerMonth(string packageType)
     {
+
         switch (packageType)
         {
             case "FREE":
@@ -28,7 +33,7 @@ public static class PermissionsHelper
             case "BASIC":
                 return 200000; // GHS2,000.00
             case "ADVANCED":
-                return null;
+                return Unlimited;
             default:
                 return 0;
         }
