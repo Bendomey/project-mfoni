3bb220d84a4ee1698324d9268643108c38d091b0
Domey Benjamin <domeybenjamin1@gmail.com>
2025-04-13 21:48:13 +0000

    feat: update S3 bucket policy to restrict access by IP and refine CORS configuration

--- infrastructure/staging/terraform/s3.tf
+++ infrastructure/staging/terraform/s3.tf
@@ -24,7 +24,6 @@ resource "aws_s3_bucket_public_access_block" "example" {
   
 }
 
-# TODO: Remove this in the future
 resource "aws_s3_bucket_policy" "staging_mfoni_bucket_policy" {
   bucket = aws_s3_bucket.staging_mfoni_bucket.id
 
@@ -38,7 +37,35 @@ resource "aws_s3_bucket_policy" "staging_mfoni_bucket_policy" {
         "Effect": "Allow",
         "Principal": "*",
         "Action": "s3:GetObject",
-        "Resource": "arn:aws:s3:::staging-mfoni/*"
+        "Resource": "arn:aws:s3:::staging-mfoni/*",
+        "Condition": {
+          "IpAddress": {
+            "aws:SourceIp": [
+              "173.245.48.0/20",
+              "103.21.244.0/22",
+              "103.22.200.0/22",
+              "103.31.4.0/22",
+              "141.101.64.0/18",
+              "108.162.192.0/18",
+              "190.93.240.0/20",
+              "188.114.96.0/20",
+              "197.234.240.0/22",
+              "198.41.128.0/17",
+              "162.158.0.0/15",
+              "104.16.0.0/13",
+              "104.24.0.0/14",
+              "172.64.0.0/13",
+              "131.0.72.0/22",
+              "2400:cb00::/32",
+              "2606:4700::/32",
+              "2803:f800::/32",
+              "2405:b500::/32",
+              "2405:8100::/32",
+              "2a06:98c0::/29",
+              "2c0f:f248::/32"
+            ]
+          }
+        }
       }
     ]
   }
@@ -51,7 +78,7 @@ resource "aws_s3_bucket_cors_configuration" "staging_mfoni_bucket_cors" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
-    allowed_origins = ["*"]
+    allowed_origins = ["https://staging-images.mfoni.app", "https://staging.mfoni.app"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
