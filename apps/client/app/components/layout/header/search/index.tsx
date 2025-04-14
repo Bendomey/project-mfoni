32800d71d80c7662c42be4969e9332f4b62b5d5c
Domey Benjamin <domeybenjamin1@gmail.com>
2025-04-14 23:48:23 +0000

    refactor: improve JSX structure and formatting in SearchPhotos component

--- apps/client/app/components/layout/header/search/index.tsx
+++ apps/client/app/components/layout/header/search/index.tsx
@@ -31,11 +31,10 @@ export const SearchPhotos = ({
 
 	return (
 		<div
-			className={`relative block w-full rounded-full border-transparent border-zinc-300 hover:border focus:ring-0 ${
-				isSearchFocused || isSittingOnADarkBackground
+			className={`relative block w-full rounded-full border-transparent border-zinc-300 hover:border focus:ring-0 ${isSearchFocused || isSittingOnADarkBackground
 					? 'border bg-white'
 					: 'bg-zinc-100'
-			}`}
+				}`}
 		>
 			<div className="flex flex-row items-center">
 				<div className="pointer-events-none inset-y-0 left-0 flex items-center pl-5">
@@ -66,43 +65,47 @@ export const SearchPhotos = ({
 
 				<div className="inset-y-0 right-0 flex items-center pr-6">
 					<Popover className="relative">
-						<Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 outline-none ring-0">
-							<FlyoutContainer
-								intendedPosition="y"
-								arrowColor="bg-blue-600"
-								initVisibility={showHelpfulInfoAboutVisualSearch}
-								onOpenInit={onCloseHelpfulInfoAboutVisualSearch}
-								FlyoutContent={
-									<div className="z-50 flex w-60 flex-col items-center justify-center rounded-2xl bg-blue-600 px-3 py-4 shadow-xl">
-										<h3 className="text-center text-sm font-bold text-white">
-											Click here to search by face
-										</h3>
-									</div>
-								}
-							>
-								<ViewfinderCircleIcon
-									className="h-5 w-5 cursor-pointer text-gray-600"
-									aria-hidden="true"
-								/>
-							</FlyoutContainer>
-						</Popover.Button>
+						{({ close }) => (
+							<>
+								<Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 outline-none ring-0">
+									<FlyoutContainer
+										intendedPosition="y"
+										arrowColor="bg-blue-600"
+										initVisibility={showHelpfulInfoAboutVisualSearch}
+										onOpenInit={onCloseHelpfulInfoAboutVisualSearch}
+										FlyoutContent={
+											<div className="z-50 flex w-60 flex-col items-center justify-center rounded-2xl bg-blue-600 px-3 py-4 shadow-xl">
+												<h3 className="text-center text-sm font-bold text-white">
+													Click here to search by face
+												</h3>
+											</div>
+										}
+									>
+										<ViewfinderCircleIcon
+											className="h-5 w-5 cursor-pointer text-gray-600"
+											aria-hidden="true"
+										/>
+									</FlyoutContainer>
+								</Popover.Button>
 
-						<Transition
-							as={Fragment}
-							enter="transition ease-out duration-200"
-							enterFrom="opacity-0 translate-y-1"
-							enterTo="opacity-100 translate-y-0"
-							leave="transition ease-in duration-150"
-							leaveFrom="opacity-100 translate-y-0"
-							leaveTo="opacity-0 translate-y-1"
-						>
-							<Popover.Panel className="absolute right-1 top-full z-30 mt-5 w-screen max-w-md overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-gray-900/5">
-								<VisualSearch
-									onClose={() => setIsSearchFocused(false)}
-									className="px-5 py-3"
-								/>
-							</Popover.Panel>
-						</Transition>
+								<Transition
+									as={Fragment}
+									enter="transition ease-out duration-200"
+									enterFrom="opacity-0 translate-y-1"
+									enterTo="opacity-100 translate-y-0"
+									leave="transition ease-in duration-150"
+									leaveFrom="opacity-100 translate-y-0"
+									leaveTo="opacity-0 translate-y-1"
+								>
+									<Popover.Panel className="absolute right-1 top-full z-30 mt-5 w-screen max-w-md overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-gray-900/5">
+										<VisualSearch
+											onClose={close}
+											className="px-5 py-3"
+										/>
+									</Popover.Panel>
+								</Transition>
+							</>
+						)}
 					</Popover>
 				</div>
 			</div>
